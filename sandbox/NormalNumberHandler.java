dcf28e74b6d26dd0a7d4e4e4cdb5ca735289d351
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-16 13:46:37 +0000

    55

--- sandbox/NormalNumberHandler.java
+++ sandbox/NormalNumberHandler.java
@@ -1,4 +1,9 @@
 public class NormalNumberHandler implements Handler {
     public NormalNumberHandler(Handler successor){
     }
+
+    @Override
+    public String handle(int number) {
+        return null;
+    }
 }

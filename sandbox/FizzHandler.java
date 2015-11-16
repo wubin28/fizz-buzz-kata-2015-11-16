56ded5c7ecfd93ae58301f92ade8c93709d2c776
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-16 13:45:56 +0000

    54

--- sandbox/FizzHandler.java
+++ sandbox/FizzHandler.java
@@ -1,4 +1,9 @@
 public class FizzHandler implements Handler {
     public FizzHandler(Handler successor) {
     }
+
+    @Override
+    public String handle(int number) {
+        return null;
+    }
 }

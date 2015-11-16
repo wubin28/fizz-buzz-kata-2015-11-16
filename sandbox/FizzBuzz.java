00e5dfef20a6b4da31f3e6f31558815fdd12c415
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-16 11:35:33 +0000

    16

--- sandbox/FizzBuzz.java
+++ sandbox/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         if (number == 1) {
             return "1";
         }
-        if (number == 5) {
+        if (number == 5 || number == 10) {
             return "buzz";
         }
         return "fizz";

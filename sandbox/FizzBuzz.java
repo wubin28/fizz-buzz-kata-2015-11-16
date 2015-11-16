71700e0a3814bf5960b52022fbdb0ef3596b2cfb
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-16 13:33:38 +0000

    39

--- sandbox/FizzBuzz.java
+++ sandbox/FizzBuzz.java
@@ -3,6 +3,7 @@ public class FizzBuzz {
     private Handler fizzBuzzHandler;
 
     public FizzBuzz() {
+        Handler fizzHandler = new FizzHandler(buzzHandler);
         this.fizzBuzzHandler = new FizzBuzzHandler(fizzHandler);
     }
 

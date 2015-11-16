b270cbfa4a8e2ca57695f8e885444311027e6612
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-16 11:12:58 +0000

    4

--- sandbox/FizzBuzzTest.java
+++ sandbox/FizzBuzzTest.java
@@ -9,4 +9,10 @@ public class FizzBuzzTest {
         int actual = FizzBuzz.answer();
         assertEquals(expected, actual);
     }
+
+    // TODO: should_print_1_if_the_number_is_1
+    // TODO: should_print_fizz_if_the_number_is_multiple_of_3
+    // TODO: should_print_buzz_if_the_number_is_multiple_of_5
+    // TODO: should_print_fizzbuzz_if_the_number_is_multiple_of_both_3_and_5
+    // TODO: should_print_100_if_the_number_is_100
 }

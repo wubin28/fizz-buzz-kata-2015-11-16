79e3adbaf7c82706e35fdb6201ca667fe207db07
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-16 11:14:28 +0000

    5

--- sandbox/FizzBuzzTest.java
+++ sandbox/FizzBuzzTest.java
@@ -4,13 +4,10 @@ import static org.junit.Assert.*;
 public class FizzBuzzTest {
 
     @Test
-    public void life_the_universe_and_everything() {
-        int expected = 42;
-        int actual = FizzBuzz.answer();
-        assertEquals(expected, actual);
+    public void should_print_1_if_the_number_is_1() {
+        assertEquals("1", fizzBuzz.print(1));
     }
 
-    // TODO: should_print_1_if_the_number_is_1
     // TODO: should_print_fizz_if_the_number_is_multiple_of_3
     // TODO: should_print_buzz_if_the_number_is_multiple_of_5
     // TODO: should_print_fizzbuzz_if_the_number_is_multiple_of_both_3_and_5

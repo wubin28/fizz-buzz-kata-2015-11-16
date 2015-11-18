0b0059806bd2d9372a6c8c5142d13fbc83ae336b
panda_EB66177979 <panda@cyber-dojo.org>
2015-11-18 15:52:42 +0000

    74

--- sandbox/FizzBuzz.java
+++ sandbox/FizzBuzz.java
@@ -6,7 +6,8 @@ public class FizzBuzz {
         Handler handlerNormalNumber = new HandlerNormalNumber(null);
         Handler handlerBuzz = new HandlerBuzz(handlerNormalNumber);
         Handler handlerFizz = new HandlerFizz(handlerBuzz);
-        this.handlerFizzBuzz = new HandlerFizzBuzz(handlerFizz);
+        Handler handlerWhizz = new HandlerWhizz(handlerFizz);
+        this.handlerFizzBuzz = new HandlerFizzBuzz(handlerWhizz);
     }
 
     public String print(int number) {
